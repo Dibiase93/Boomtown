9faec300f978fca200c47cc4f1454093fbba2055
Andrew DiBiase <dibiase.andrew@outlook.com>
2019-07-17 13:33:13 -0700

    add route url and nav

--- client/src/pages/Items/ItemsContainer.js
+++ client/src/pages/Items/ItemsContainer.js
@@ -3,7 +3,7 @@ import Items from "./Items";
 // import FullScreenLoader from "../../components/FullScreenLoader";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
-// import { } from '../../apollo/queries';
+import { ALL_ITEMS_QUERY } from "../../apollo/queries";
 
 const GET_TAGS = gql`
   {
@@ -13,25 +13,25 @@ const GET_TAGS = gql`
   }
 `;
 
-// const GET_ITEMS = (ItemFields.fragment = {
-//   entry: gql`
-//     fragment ItemFields on Entry {
-//       items
-//     }
-//   `
-// });
-
 class ItemsContainer extends Component {
   render() {
     return (
-      <Query query={GET_TAGS}>
-        {({ loading, error, data }) => {
-          if (loading) return "Loading...";
-          if (error) return `Error! ${error.message}`;
-          console.log(data);
-          return <Items tags={data.tags} />;
-        }}
-      </Query>
+      <div>
+        <Query query={ALL_ITEMS_QUERY} variables={{ filter: 1 }}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+            return <Items items={data.items.title} />;
+          }}
+        </Query>
+        <Query query={GET_TAGS}>
+          {({ loading, error, data }) => {
+            if (loading) return "Loading...";
+            if (error) return `Error! ${error.message}`;
+            return <Items tags={data.tags} />;
+          }}
+        </Query>
+      </div>
     );
   }
 }
--- client/src/routes/index.js
+++ client/src/routes/index.js
@@ -1,18 +1,26 @@
-import React, { Fragment } from 'react';
-import { Redirect, Route, Switch } from 'react-router';
+import React, { Fragment } from "react";
+import { Redirect, Route, Switch } from "react-router";
 
 export default () => (
   <Fragment>
-    {/* @TODO: Add your menu component here */}
-    <Switch>
-      {/**
-       * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
-       *
-       * Provide a wildcard redirect to /items for any undefined route using <Redirect />.
-       *
-       * Later, we'll add logic to send users to one set of routes if they're logged in,
-       * or only view the /welcome page if they are not.
-       */}
-    </Switch>
+    <Rounter>
+      {/* @TODO: Add your menu component here */}
+      <NavLinks />
+      <Switch>
+        {/**
+         * @TODO: Define routes here for: /items, /profile, /profile/:userid, and /share
+         *
+         * Provide a wildcard redirect to /items for any undefined route using <Redirect />.
+         *
+         * Later, we'll add logic to send users to one set of routes if they're logged in,
+         * or only view the /welcome page if they are not.
+         */}
+        <Route path="/items" />
+        <Redirect path="undefined" to="items" />
+        <Route path="/profile" exact />
+        <Route path="/profile/:userid" />
+        <Route path="/share" />
+      </Switch>
+    </Rounter>
   </Fragment>
 );
